3855c2d3a4ca2c4559fb069dff661dbb383596ef
Max <mjann@ucsc.edu>
2019-06-23 05:39:20 -0700

    jesus

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import marble from './marble.png';
 
 import './App.css';
@@ -8,6 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome</h1>
+        <h3>Click on the game to play, music to listen, or scroll down to read.</h3>
         <a href="https://codejoy-wins.itch.io/explorer">
           <img src={marble} className="App-logo" alt="logo" />
         </a>
@@ -17,8 +18,31 @@ function App() {
         <iframe id="vid" width="560" height="315" src="https://www.youtube.com/embed/jeo3an2M_Lo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </header>
       <h2>Some Space</h2>
-      <p>I like react because I like making apps quickly from scratch. The problem is that I don't remember all the little tricks I've learned.  One thing I've done is make the logo a different svg file.  I guess I can just use an img tag and then make it spin by giving it a class of app-logo.</p>
-      <p>I want to use best practices but it's hard to remember everything.  Hopefully this looks nice.</p>
+      <p>You are dead. The trial will begin shortly. The Devil is the prosecutor, and Jesus Christ is your defense attorney. God will be the judge, and no superpowers are allowed in his courtroom. </p><p>
+
+      The courtroom is similar to an American styled courtroom but glistening with white and gold.  
+      </p><p>
+      
+      
+      “All rise” - Said the Lord thy God
+      
+      
+      </p><p>
+      Everyone stands up and does whatever people do in your current time so you can understand the process.
+      
+      </p><p>
+      
+      Jesus goes over your good deeds and how you are innocent until proven guilty. 
+      </p><p>
+      The Devil goes into detail about all the horrible things you’ve done and thought about doing if only you had the power you’d have in heaven.
+      </p><p>
+      Jesus forgives you and defends you until the end of time.
+      </p><p>
+      God does not forgive, but is just. He listens to both the devil and Jesus, and his jury of angels.  
+      
+      Then he determines whether you go to heaven or hell or something else. 
+      
+      How do you think this trial will go for you?  </p>
     <a href="https://maxjann.com">MJ</a>
     </div>
   );
