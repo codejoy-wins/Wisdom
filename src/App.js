508fe960ce78a4ca568ccae61be73535a0c9233e
Max <mjann@ucsc.edu>
2019-06-22 17:23:55 -0700

    game add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import marble from './marble.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="https://codejoy-wins.itch.io/explorer">
+          <img src={marble} className="App-logo" alt="logo" />
         </a>
+          <p>
+          k and m to rise and fall.  wasd to move left, right, up and down.
+          </p>
+        <iframe id="vid" width="560" height="315" src="https://www.youtube.com/embed/jeo3an2M_Lo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </header>
+      <h2>Some Space</h2>
+      <p>I like react because I like making apps quickly from scratch. The problem is that I don't remember all the little tricks I've learned.  One thing I've done is make the logo a different svg file.  I guess I can just use an img tag and then make it spin by giving it a class of app-logo.</p>
+      <p>I want to use best practices but it's hard to remember everything.  Hopefully this looks nice.</p>
+
     </div>
   );
 }
